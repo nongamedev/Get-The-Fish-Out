755ddf22674bd9ce89de174960a2d6ad7a858fb9
nongamedev@gmail.com <non.gamedev@gmail.com>
2022-11-12 13:13:29 +0700

    Done Cat Attack anim

--- Assets/Scripts/OnTriggerEvent.cs
+++ Assets/Scripts/OnTriggerEvent.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.Events;
 public class OnTriggerEvent : MonoBehaviour
 {
-
     [SerializeField] private UnityEvent onTriggerEvent;
 
     private void OnTriggerEnter2D(Collider2D collision)
